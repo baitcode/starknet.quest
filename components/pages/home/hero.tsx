29b21bcf8ffa9ca38063e3de576cbd81d7c9af5d
baitcode <batiyiv@gmail.com>
2024-11-27 00:51:46 +0300

    some sinplifications

--- components/pages/home/hero.tsx
+++ components/pages/home/hero.tsx
@@ -5,31 +5,27 @@ import Typography from "../../UI/typography/typography";
 import { TEXT_TYPE } from "@constants/typography";
 import SplitTitle from "./splitTitle";
 
-type HeroProps = {
-};
+type HeroProps = {};
 
-const Hero: FunctionComponent<HeroProps> = ({
-}) => {
+const Hero: FunctionComponent<HeroProps> = ({}) => (
+  <div>
+      <div className="item-center justify-center pb-4">
+        <SplitTitle className="leading-12 pb-2" />
 
-  return (
-    <div>
-        <div className="item-center justify-center pb-4">
-          <SplitTitle className="leading-12 pb-2" />
-
-          <Typography type={TEXT_TYPE.H1} 
-            className={`${styles.header} flex justify-center text-center text-5xl leading-12 text-transparent`}>
-            WITH STARKNET
-          </Typography>
-        </div>
-        
-        <Typography type={TEXT_TYPE.BODY_MICRO} color="textGray"
-          className="max-w-screen-sm text-sm leading-6 text-center">
-          Starknet Quest is our gateway to DeFi, offering tools and opportunities 
-          for individuals, DAOs, and protocols to grow their assets and explore Starknet's
-          full potential.
+        <Typography type={TEXT_TYPE.H1} 
+          className={`${styles.header} flex justify-center text-center text-5xl leading-12 text-transparent`}>
+          WITH STARKNET
         </Typography>
+      </div>
+      
+      <Typography type={TEXT_TYPE.BODY_MICRO} color="textGray"
+        className="max-w-screen-sm text-sm leading-6 text-center">
+        Starknet Quest is our gateway to DeFi, offering tools and opportunities 
+        for individuals, DAOs, and protocols to grow their assets and explore Starknet's
+        full potential.
+      </Typography>
 
-    </div>
-  );
-};
+  </div>
+);
+
 export default Hero;
